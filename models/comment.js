fc665ffaddf1be9e5b8d804a8add24baa97ab586
shishir singh <shishirsinghvaranasi@gmail.com>
2021-02-10 11:51:31 +0530

    create schema for comments

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const commentSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    // comment belong to a user
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User' // ref option is what tells Mongoose which model to use during population. Here it is referring to User model
+    },
+    // comment belong to a post
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'post'
+    }
+},{
+    timestamps:true
+});
+
+const Post = mongoose.model('Post',postSchema);
+module.exports = Post;
--- models/post.js
+++ models/post.js
@@ -7,7 +7,17 @@ const postSchema = new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User' // ref option is what tells Mongoose which model to use during population. Here it is referring to User model
-    }
+    },
+         // include array of ids of all comments in post schema itself
+         // We'll be loading comments alongside every post. So in order to fetch the comments of a particular post(from comment collection) we're storing the comment ids in comment array.
+         // This would make accessing the comments easy and much faster 
+    comment:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'comment'
+        }
+    ]
+
 },{
     timestamps:true
 });
