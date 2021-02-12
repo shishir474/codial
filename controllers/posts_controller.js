c7449d51b05e56381745f45a6a2ada49ccc33b0c
shishir singh <shishirsinghvaranasi@gmail.com>
2021-02-12 17:12:34 +0530

    delete posts with authorization checks enabled

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,7 @@
+const { comment } = require("./comments_controller");
+
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.postRender = function(req,res){
     res.end('<h1>rendering your Posts..wait for a moment please!!</h1>')
@@ -25,4 +28,34 @@ module.exports.createpost = function(req,res){
         console.log('********',newPost);
        return res.redirect('back');
     });
+}
+
+// deleting posts
+
+module.exports.destroy = function(req,res){
+    //  route will be /posts/destroy/id   id => string params
+    // finding the post in db using id paramater that is a part of the url before deleting
+    // we access string params using req.params
+    Post.findById(req.params.id, function(err,post){
+        // post found 
+        // authorization part:  Am I authorized to delete the post posted by someone else.. Obviously not. So check whether the user who is deleting the post is the same user who has created the post
+        // post.user is an id of the user(until and unless I populate). It returns string id
+        // When I'm compairing id's of two objects I need to covert them both in string format
+        // Ideally we should be using req.user._id but we're using .id.
+        // .id means converting the object id into string
+        if (post.user == req.user.id){
+            //removepost
+            post.remove();
+
+            // deleting comments. For this I need to import comment model
+            // deleteMany function deletes all the comments based on some queries passed
+            Comment.deleteMany({post:req.params.id}, function(err){
+                return res.redirect('back');
+            });
+
+        }else{
+            // user doesn't match so cant delete
+            return res.redirect('back');
+        }
+    })
 }
